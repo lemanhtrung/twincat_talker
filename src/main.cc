9d859e04cb363bf2c0bedc269e0d6804753f705b
rehrler <romehrler@gmail.com>
2020-06-11 17:26:32 +0200

    simple example

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
 #include "AdsLib.h"
+#include "AdsNotification.h"
+#include "AdsVariable.h"
 #include "ros/ros.h"
 
 int main(int argc, char *argv[]) {
@@ -7,6 +9,15 @@ int main(int argc, char *argv[]) {
   ros::NodeHandle n;
 
   ROS_INFO_STREAM("running good");
+
+  static const AmsNetId remoteNetId{10, 180, 30, 120, 1, 1};
+  static const char remoteIpV4[] = "ads-server";
+
+  AdsDevice route{remoteIpV4, remoteNetId, AMSPORT_R0_PLC_TC3};
+
+  AdsVariable<float_t> variable_to_write{route, "MAIN.myVar"};
+
+  variable_to_write = 4.0f;
 
   return 0;
 }
