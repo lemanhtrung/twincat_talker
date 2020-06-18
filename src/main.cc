fd99bbf17a2c092515a8ee3fc2891153826bd819
rehrler <romehrler@gmail.com>
2020-06-18 19:01:31 +0200

    small adj

--- src/main.cc
+++ src/main.cc
@@ -10,14 +10,22 @@ int main(int argc, char *argv[]) {
 
   ROS_INFO_STREAM("running good");
 
-  static const AmsNetId remoteNetId{10, 180, 30, 120, 1, 1};
-  static const char remoteIpV4[] = "ads-server";
+  static const AmsNetId remoteNetId{5, 67, 39, 56, 1, 1};
+  static const char remoteIpV4[] = "10.180.30.49";
 
   AdsDevice route{remoteIpV4, remoteNetId, AMSPORT_R0_PLC_TC3};
 
-  AdsVariable<float_t> variable_to_write{route, "MAIN.myVar"};
+  AdsVariable<double> variable_to_write{route, "MAIN.myFloatVAR"};
+  
+  ros::Rate loop_rate(0.1);
 
-  variable_to_write = 4.0f;
+  while (ros::ok())
+  {
+  	variable_to_write = 12.0;
+	ROS_INFO_STREAM("changed variable on sps");
+	ros::spinOnce();
+	loop_rate.sleep();
+  }
 
   return 0;
 }
