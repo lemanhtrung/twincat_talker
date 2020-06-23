fbbcc22a2ef463d4b11b02694b38f1c5ecc31766
rehrler <romehrler@gmail.com>
2020-06-23 14:04:48 +0200

    setup for bozzini

--- src/main.cc
+++ src/main.cc
@@ -10,22 +10,17 @@ int main(int argc, char *argv[]) {
 
   ROS_INFO_STREAM("running good");
 
-  static const AmsNetId remoteNetId{5, 67, 39, 56, 1, 1};
-  static const char remoteIpV4[] = "10.180.30.49";
+  static const AmsNetId remoteNetId{5, 57, 164, 100, 1, 1};
+  static const char remoteIpV4[] = "10.180.30.220";
 
   AdsDevice route{remoteIpV4, remoteNetId, AMSPORT_R0_PLC_TC3};
 
-  AdsVariable<double> variable_to_write{route, "MAIN.myFloatVAR"};
-  
-  ros::Rate loop_rate(0.1);
+  AdsVariable<double> variable_to_write{route, "Bozzini_Masterarbeit.X_Pick"};
+  AdsVariable<bool> control{route, "Bozzini_Masterarbeit.Send_Coordinates"};
 
-  while (ros::ok())
-  {
-  	variable_to_write = 12.0;
-	ROS_INFO_STREAM("changed variable on sps");
-	ros::spinOnce();
-	loop_rate.sleep();
-  }
+  variable_to_write = 20.0;
+  control = false;
+  ROS_INFO_STREAM("changed variable on sps");
 
   return 0;
 }
